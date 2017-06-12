6c789760daa0766adf24a3c3174d8d23bf6f143e
Pankajashree <pankajashree@gmail.com>
2017-06-12 19:28:02 +0530

    change variable name in dishdetail.html

--- confusion_gulp/app/scripts/controllers.js
+++ confusion_gulp/app/scripts/controllers.js
@@ -132,18 +132,14 @@ angular.module('confusionApp')
             
             $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             
-            $scope.submitComment = function () {
-                
-                $scope.mycomment.date = new Date().toISOString();
+              $scope.submitComment = function () {
+                                $scope.mycomment.date = new Date().toISOString();
                 console.log($scope.mycomment);
-                
-                $scope.dish.comments.push($scope.mycomment);
+                                $scope.dish.comments.push($scope.mycomment);
 
-                menuFactory.getDishes().update({id: $scope.dish.id}, $scope.dish);
-                
-                $scope.commentForm.$setPristine();
-                
-                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+                menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
+                                $scope.commentForm.$setPristine();
+                                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             };
         }])
 
